f99585a7db4e1288770ddbc240681d4bcf38e6cc
arapbaeva <87788835+arapbaeva@users.noreply.github.com>
2022-01-24 17:23:23 +0600

    first commit

---
+++ JS_0029/script.js
@@ -0,0 +1,11 @@
+function func(){
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    let result = (num1) + (num2)
+
+    document.getElementById("result").innerHTML = result;
+}
+
+
+
